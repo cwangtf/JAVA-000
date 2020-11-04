8dc959778fbd55dc0a4d363de3a60f47cd447a7d
xing_seng <xing_seng@hunliji.com>
2020-11-04 23:55:20 +0800

    第三周

---
+++ Week_03/nio02/src/main/java/io/github/kimmking/gateway/outbound/httpclient/HttpClientOutboundHandler.java
@@ -0,0 +1,97 @@
+package io.github.kimmking.gateway.outbound.httpclient;
+
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.http.DefaultFullHttpResponse;
+import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.FullHttpResponse;
+import io.netty.handler.codec.http.HttpUtil;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+
+import java.io.IOException;
+
+import static io.netty.handler.codec.http.HttpResponseStatus.NO_CONTENT;
+import static io.netty.handler.codec.http.HttpResponseStatus.OK;
+import static io.netty.handler.codec.http.HttpVersion.HTTP_1_1;
+
+/**
+ * @author wangtf
+ * @date 2020/11/4
+ */
+public class HttpClientOutboundHandler {
+
+    private CloseableHttpClient httpClient;
+    private String backendUrl;
+
+    public HttpClientOutboundHandler(String backendUrl) {
+        this.backendUrl = backendUrl.endsWith("/")?backendUrl.substring(0,backendUrl.length()-1):backendUrl;
+        httpClient = HttpClients.createDefault();
+    }
+
+    public void handle(final FullHttpRequest fullHttpRequest, final ChannelHandlerContext channelHandlerContext) {
+        final String url = this.backendUrl + fullHttpRequest.uri();
+        fetchGet(fullHttpRequest, channelHandlerContext, url);
+    }
+
+    private void fetchGet(final FullHttpRequest inbound, final ChannelHandlerContext channelHandlerContext, final String url) {
+        final HttpGet httpGet = new HttpGet(url);
+        // Create a custom response handler
+        ResponseHandler<String> responseHandler = new ResponseHandler<String>() {
+
+            @Override
+            public String handleResponse(
+                    final HttpResponse response) throws ClientProtocolException, IOException {
+                try {
+                    response(inbound, channelHandlerContext, response);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return null;
+            }
+
+        };
+        try {
+            httpClient.execute(httpGet, responseHandler);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void response(final FullHttpRequest fullRequest, final ChannelHandlerContext ctx, final HttpResponse endpointResponse) throws Exception {
+        FullHttpResponse response = null;
+        try {
+            byte[] body = EntityUtils.toByteArray(endpointResponse.getEntity());
+
+            response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.wrappedBuffer(body));
+            response.headers().set("Content-Type", "application/json");
+            response.headers().setInt("Content-Length", Integer.parseInt(endpointResponse.getFirstHeader("Content-Length").getValue()));
+        } catch (Exception e) {
+            e.printStackTrace();
+            response = new DefaultFullHttpResponse(HTTP_1_1, NO_CONTENT);
+            exceptionCaught(ctx, e);
+        } finally {
+            if (fullRequest != null) {
+                if (!HttpUtil.isKeepAlive(fullRequest)) {
+                    ctx.write(response).addListener(ChannelFutureListener.CLOSE);
+                } else {
+                    ctx.write(response);
+                }
+            }
+            ctx.flush();
+        }
+    }
+
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+}
